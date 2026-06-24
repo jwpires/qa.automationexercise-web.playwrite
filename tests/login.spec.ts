1d14990b835568dd9fd6e9e646f117fe5c98ed37
Jose <jose.weverton.uni@gmail.com>
2026-06-23 23:40:35 -0300

    Versão atualizada e melhorada para teste de Login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,8 +3,8 @@ import { LoginPage } from "../class/LoginPage";
 
 test("[CT-01] - Realizar login com sucesso na plataforma", async ({ page }) => {
   const loginPage = new LoginPage(page);
-  await loginPage.accessUrl("https://automationexercise.com/login");
+  await loginPage.goto();
   await loginPage.login("jose.pires@bol.com", "123456");
 
-  await expect(page).toHaveTitle(/Logged in as jose/);
+  await expect(page.getByText(/Logged in as jose/)).toBeVisible();
 });
